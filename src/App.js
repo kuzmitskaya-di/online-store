edd5c0ef90489f62ad2dffa134594f165a0b5eb1
diana_kuz <kuzmitskaya.di@gmail.com>
2020-12-10 18:58:00 +0300

    Add - Navbar.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./Static/Header/Header";
+import Navbar from "./Static/Navbar/Navbar";
+
 
 function App() {
   return (
     <div className="app-wrapper">
       <Header />
+      <Navbar />
     </div>
   );
 }
--- src/Static/Navbar/Navbar.js
+++ src/Static/Navbar/Navbar.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import s from './Navbar.module.css';
 
-const Header = () =>{
+const Navbar = () =>{
     return(
-        <nav className={}>
-            <div>
-
+        <nav className={s.navbar}>
+            <div className={s.logo}>
+                <span className={s.navbar_logo}><span className={s.navbar_logo_green}>RENOSHOP</span>BEE</span>
             </div>
-            <menu>
-
-            </menu>
-            <nav>
-
+            <div className={s.menu}>
+                <a href="#" className={`${s.menu_link} ${s.menu_link_green}`}>HOME</a>
+                <a href="#" className={s.menu_link}>WOMEN</a>
+                <a href="#" className={s.menu_link}>MEN</a>
+                <a href="#" className={s.menu_link}>KIDS</a>
+                <a href="#" className={s.menu_link}>JEWELLERY</a>
+                <a href="#" className={s.menu_link}>ACCESSORIES</a>
+            </div>
+            <nav className={s.navigation_button}>
+                <a href="#" className={`${s.nav_but} ${s.basket}`}></a>
+                <a href="#" className={`${s.nav_but} ${s.search}`}></a>
+                <a href="#" className={`${s.nav_but} ${s.burger_menu}`}></a>
             </nav>
         </nav>
     );
 }
 
-export default Header;
+export default Navbar;
